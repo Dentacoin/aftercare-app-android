bf0dfd18cd87653fe994854c46bc1ba94d80ce21
Atanas Chervarov <atanaschervarov@gmail.com>
2017-07-26 15:49:44 +0300

    - adding product flavors - adding debug buildtype - setting up hockey

--- app/src/main/java/com/dentacoin/dentacare/LaunchActivity.java
+++ app/src/main/java/com/dentacoin/dentacare/LaunchActivity.java
@@ -1,7 +1,12 @@
 package com.dentacoin.dentacare;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dentacoin.dentacare.utils.DCConstants;
+
+import net.hockeyapp.android.CrashManager;
+import net.hockeyapp.android.UpdateManager;
 
 public class LaunchActivity extends AppCompatActivity {
 
@@ -9,5 +14,42 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        checkForUpdates();
+    }
+
+    private void checkForCrashes() {
+        // Send crash reports only for release versions
+        if (BuildConfig.BUILD_TYPE.equals(DCConstants.BUILD_TYPE_RELEASE)) {
+            CrashManager.register(this);
+        }
+    }
+
+    private void checkForUpdates() {
+        // Remove this for store builds
+        if (!BuildConfig.FLAVOR.equals(DCConstants.BUILD_FALVOR_LIVE) && !BuildConfig.BUILD_TYPE.equals(DCConstants.BUILD_TYPE_DEBUG)) {
+            UpdateManager.register(this);
+        }
+    }
+
+    private void unregisterManagers() {
+        UpdateManager.unregister();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        checkForCrashes();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterManagers();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterManagers();
     }
 }
---
+++ app/src/main/java/com/dentacoin/dentacare/utils/DCConstants.java
@@ -0,0 +1,13 @@
+package com.dentacoin.dentacare.utils;
+
+/**
+ * Created by Atanas Chervarov on 7/26/17.
+ */
+
+public class DCConstants {
+    public static final String BUILD_FLAVOR_DEV = "dev";
+    public static final String BUILD_FLAVOR_STAGING = "staging";
+    public static final String BUILD_FALVOR_LIVE = "live";
+    public static final String BUILD_TYPE_DEBUG = "debug";
+    public static final String BUILD_TYPE_RELEASE = "release";
+}
