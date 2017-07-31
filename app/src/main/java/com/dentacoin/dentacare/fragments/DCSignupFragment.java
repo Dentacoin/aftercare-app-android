f29b5a1823fbf11d3f0dbdcd0aea127f613901b6
Atanas Chervarov <atanaschervarov@gmail.com>
2017-07-31 22:20:48 +0300

    - using TextInputEditText instead of EditText

--- app/src/main/java/com/dentacoin/dentacare/fragments/DCSignupFragment.java
+++ app/src/main/java/com/dentacoin/dentacare/fragments/DCSignupFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.dentacoin.dentacare.R;
+import com.facebook.drawee.view.SimpleDraweeView;
 
 /**
  * Created by Atanas Chervarov on 7/29/17.
@@ -17,12 +18,16 @@ public class DCSignupFragment extends DCFragment implements View.OnClickListener
     public static final String TAG = DCSignupFragment.class.getSimpleName();
 
     private ImageView ivSignupBack;
+    private SimpleDraweeView sdvSignupProfileImage;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
         View view = inflater.inflate(R.layout.fragment_signup, container, false);
 
         ivSignupBack = (ImageView) view.findViewById(R.id.iv_signup_back);
         ivSignupBack.setOnClickListener(this);
+        sdvSignupProfileImage = (SimpleDraweeView) view.findViewById(R.id.sdv_signup_profile_image);
+        sdvSignupProfileImage.setOnClickListener(this);
 
         return view;
     }
@@ -33,6 +38,13 @@ public class DCSignupFragment extends DCFragment implements View.OnClickListener
             case R.id.iv_signup_back:
                 onBackPressed();
                 break;
+            case R.id.sdv_signup_profile_image:
+                pickAvatar();
+                return;
         }
+    }
+
+    private void pickAvatar() {
+        //TODO: pickup picture from gallery
     }
 }
