d0a41b816d6dc81cf4799b8bb0f06c5957013647
Atanas Chervarov <atanaschervarov@gmail.com>
2018-07-09 16:14:34 +0300

    - new handling for firebase notifications & inhouse notifications

--- app/src/main/java/com/dentacoin/dentacare/firebase/DCFirebaseMessagingService.java
+++ app/src/main/java/com/dentacoin/dentacare/firebase/DCFirebaseMessagingService.java
@@ -1,15 +1,17 @@
 package com.dentacoin.dentacare.firebase;
 
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import com.dentacoin.dentacare.LaunchActivity;
 import com.dentacoin.dentacare.R;
+import com.dentacoin.dentacare.utils.DCConstants;
 import com.dentacoin.dentacare.utils.DCSharedPreferences;
+import com.dentacoin.dentacare.utils.DCUtils;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -43,17 +45,21 @@ public class DCFirebaseMessagingService extends FirebaseMessagingService {
                 message = remoteMessage.getNotification().getBody();
             }
 
+            DCUtils.createNotificationChannel(this, DCConstants.NOTIFICATION_CHANNEL_ID);
 
             PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, DCConstants.NOTIFICATION_CHANNEL_ID)
                     .setContentTitle(title)
+                    .setContentText(message)
                     .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
                     .setSmallIcon(R.drawable.notification_icon)
                     .setAutoCancel(true)
                     .setSound(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.notification))
-                    .setContentIntent(pendingIntent);
+                    .setContentIntent(pendingIntent)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH);
 
-            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
             notificationManager.notify(DCSharedPreferences.getNotificationId(), notificationBuilder.build());
         }
     }
